aab0868352150a07342de8363a7d6832c1b7713d
Duco Dokter <dokter@w20e.com>
2012-08-14 12:49:23 +0200

    Moved news to nature

---
+++ w20e/pycms_news/interfaces.py
@@ -0,0 +1,6 @@
+from w20e.pycms.interfaces import INature
+
+
+class INews(INature):
+
+    """ Marker for news nature"""
--- w20e/pycms_news/views/news.py
+++ w20e/pycms_news/views/news.py
@@ -1,6 +1,7 @@
 from repoze.catalog.query import Eq
 from pyramid.url import resource_url
 from w20e.pycms.views.base import ContentView
+from w20e.pycms_news.interfaces import INews
 
 
 class NewsListingMixin:
@@ -9,19 +10,18 @@ class NewsListingMixin:
 
         cat = self.context.root._catalog
 
-        res = cat.query(Eq('ctype', 'news'))
+        res = cat.query(Eq('nature', "w20e.pycms_news.interfaces.INews"))
 
         objs = []
 
         for result in res[1]:
             obj = cat.get_object(result)
-
-            if obj:
-                objs.append({"title": obj.title,
-                             "date": obj.created.strftime('%d-%m-%Y'),
-                             "sortdate": obj.created.strftime('%Y%m%d'),
-                             "url": resource_url(obj, self.request),
-                             "text": obj.text})
+            
+            objs.append({"title": obj.title,
+                         "date": obj.created.strftime('%d-%m-%Y'),
+                         "sortdate": obj.created.strftime('%Y%m%d'),
+                         "url": resource_url(obj, self.request),
+                         "text": obj.full_text})
 
         def newest_sort(a, b):
 
@@ -34,19 +34,30 @@ class NewsListingMixin:
 
 class NewsViewlet(object, NewsListingMixin):
 
+    """ Fetch news and provide to viewlet"""
+
     def __init__(self, context, request):
 
         self.context = context
         self.request = request
 
         self.limit = self.request.get('kwargs', {}).get('limit', 5)
+        self._news = self.listnews()
 
     def __call__(self):
 
-        news = self.listnews()
+        return {}
 
-        return {'newslisting': news[:self.limit]}
-        
+    @property
+    def news(self):
+
+        return self._news[:self.limit]
+
+    @property
+    def has_more_news(self):
+
+        return len(self._news) > self.limit
+
 
 class NewsView(ContentView):
 
